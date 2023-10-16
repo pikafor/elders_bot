f07545329f78065ac1b97b61ab76cd972ae0741f
pikafor <DAUNBLAT228@yandex.ru>
2023-10-16 21:23:43 +0300

    commit

---
+++ dispatcher/src/main/java/ru/relex/controller/TelegramBot.java
@@ -0,0 +1,21 @@
+package ru.relex.controller;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class TelegramBot extends TelegramLongPollingBot {
+    @Override
+    public String getBotUsername() {
+        return null;
+    }
+
+    @Override
+    public String getBotToken() {
+        return null;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+    }
+}
