a2451ffde2d0d147f008404c6a54cb2734b3b327
pikafor <DAUNBLAT228@yandex.ru>
2023-10-16 21:35:59 +0300

    commit

--- dispatcher/src/main/java/ru/relex/controller/TelegramBot.java
+++ dispatcher/src/main/java/ru/relex/controller/TelegramBot.java
@@ -1,21 +1,24 @@
 package ru.relex.controller;
 
+import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
+@Component
 public class TelegramBot extends TelegramLongPollingBot {
     @Override
     public String getBotUsername() {
-        return null;
+        return "dgtu_elders_bot";
     }
 
     @Override
     public String getBotToken() {
-        return null;
+        return "6645254379:AAGJfYfJ_AZmFFHoawmkvZhoqieiZCQnh7I";
     }
 
     @Override
     public void onUpdateReceived(Update update) {
-
+        var originalMessage = update.getMessage();
+        System.out.println(originalMessage.getText());
     }
 }
